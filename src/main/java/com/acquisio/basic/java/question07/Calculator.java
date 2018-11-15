bc83addd1c6154e18bdcf05251ca8f2ccc0039fa
shumingzhao <symonzhao2016@gmail.com>
2018-11-14 20:15:08 -0800

    Update Calculator.java

--- src/main/java/com/acquisio/basic/java/question07/Calculator.java
+++ src/main/java/com/acquisio/basic/java/question07/Calculator.java
@@ -27,5 +27,75 @@ public class Calculator {
                     String.format("Operator '%s' not supported", operator));
         }
     }
+    @Before
+	public void setUp() {
+		System.out.println("---begin test---");
+	}
+    //add
+    @Test(timeout=1000)
+    public void unitTestAdd(){
+    	System.out.println("=Add="+applyOperation(-1,-1,'+'));
+    	System.out.println("=Add="+applyOperation(-1,0,'+'));
+    	System.out.println("=Add="+applyOperation(-1,1,'+'));
+    	System.out.println("=Add="+applyOperation(0,-1,'+'));
+    	System.out.println("=Add="+applyOperation(0,0,'+'));
+    	System.out.println("=Add="+applyOperation(0,1,'+'));
+    	System.out.println("=Add="+applyOperation(1,-1,'+'));
+    	System.out.println("=Add="+applyOperation(1,0,'+'));
+    	System.out.println("=Add="+applyOperation(1,1,'+'));
+    }
+    //sub
+    @Test(timeout=1000)
+    public void unitTestSub(){
+    	System.out.println("=Sub="+applyOperation(-1,-1,'-'));
+    	System.out.println("=Sub="+applyOperation(-1,0,'-'));
+    	System.out.println("=Sub="+applyOperation(-1,1,'-'));
+    	System.out.println("=Sub="+applyOperation(0,-1,'-'));
+    	System.out.println("=Sub="+applyOperation(0,0,'-'));
+    	System.out.println("=Sub="+applyOperation(0,1,'-'));
+    	System.out.println("=Sub="+applyOperation(1,-1,'-'));
+    	System.out.println("=Sub="+applyOperation(1,0,'-'));
+    	System.out.println("=Sub="+applyOperation(1,1,'-'));
+    }
+    //mul
+    @Test(timeout=1000)
+    public void unitTestMul(){
+    	System.out.println("=Mul="+applyOperation(-1,0,'*'));
+    	System.out.println("=Mul="+applyOperation(-1,1,'*'));
+    	System.out.println("=Mul="+applyOperation(-1,-1,'*'));
+    	System.out.println("=Mul="+applyOperation(0,0,'*'));
+    	System.out.println("=Mul="+applyOperation(0,1,'*'));
+    	System.out.println("=Mul="+applyOperation(0,-1,'*'));
+    	System.out.println("=Mul="+applyOperation(1,-1,'*'));
+    	System.out.println("=Mul="+applyOperation(1,0,'*'));
+    	System.out.println("=Mul="+applyOperation(1,1,'*'));
+    }
+    //div
+    @Test(timeout=1000)
+    public void unitTestDiv(){
+    	System.out.println("=Div="+applyOperation(-1,-1,'/'));
+    	System.out.println("=Div="+applyOperation(-1,1,'/'));
+    	System.out.println("=Div="+applyOperation(0,-1,'/'));
+    	System.out.println("=Div="+applyOperation(0,1,'/'));
+    	System.out.println("=Div="+applyOperation(1,-1,'/'));
+    	System.out.println("=Div="+applyOperation(1,1,'/'));
+    }
+    @Test(expected=java.lang.ArithmeticException.class)
+    public void expTest(){
+    	System.out.println("=Div="+applyOperation(-1,0,'/'));
+    	System.out.println("=Div="+applyOperation(0,0,'/'));
+    	System.out.println("=Div="+applyOperation(1,0,'/'));
+    	
+    	System.out.println("=Other="+applyOperation(1,1,'%'));
+    	System.out.println("=Other="+applyOperation(1,1,'='));
+    	System.out.println("=Other="+applyOperation(1,1,'!'));
+    	System.out.println("=Other="+applyOperation(1,1,'&'));
+    	System.out.println("=Other="+applyOperation(1,1,'^'));
+    	
+    }
+    @After
+	public void tearDown() {
+		System.out.println("---end test---");
+	}
 
 }
