9d5190ff04fbc39e08db6f5374144875af739861
shumingzhao <symonzhao2016@gmail.com>
2018-11-18 13:31:36 -0800

    Update Vegetable.java

--- src/main/java/com/acquisio/basic/java/question04/Vegetable.java
+++ src/main/java/com/acquisio/basic/java/question04/Vegetable.java
@@ -2,17 +2,11 @@ package com.acquisio.basic.java.question04;
 
 class Vegetable extends Item {
 
-    private String type;
     private String origin;
 
     Vegetable(String name, String type, String origin, int quantity, double price) {
-        super(name, quantity, price);
-        this.type = type;
+        super(name, quantity, price, type);
         this.origin = origin;
-    }
-
-    public String getType() {
-        return type;
     }
 
     public String getOrigin() {
