b3f5c44310efb029462fd754855c0cff2076ad58
shumingzhao <symonzhao2016@gmail.com>
2018-11-18 11:49:57 -0800

    Update Refactoring.java

--- src/main/java/com/acquisio/basic/java/question05/Refactoring.java
+++ src/main/java/com/acquisio/basic/java/question05/Refactoring.java
@@ -105,61 +105,49 @@ package com.acquisio.basic.java.question05;
  */
 public class Refactoring {
     Item[] items;
+    final String Aged = "Aged Brie";
+    final String Backstage = "Backstage passes to a TAFKAL80ETC concert";
+    final String Sulfuras = "Sulfuras, Hand of Ragnaros";
 
     public Refactoring(Item[] items) {
         this.items = items;
     }
 
-    public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
-            } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
-            }
-        }
+    public void updateQuality(){
+    	for(int i=0;i<items.length;i++){
+    		String name = items[i].name;
+    		int quality = items[i].quality;
+    		int sellIn = items[i].sellIn;
+    		
+    		if(name.equals(Aged)){
+    			quality = (quality < 50 == true)? quality+1 : quality;
+    		}else if(name.equals(Backstage)){
+    			if(quality < 50){
+    				if(sellIn >= 6 && sellIn <11){
+    					quality = quality+2;
+    				}else if(sellIn<6){
+    					quality = quality+3;
+    				}
+    			}
+    		}else if(!name.equals(Sulfuras)){
+    				quality = quality+1;
+    		}
+    		if(!name.equals(Sulfuras)){
+    				sellIn =sellIn-1;
+    		}
+    		if(sellIn<0){
+    			if(name.equals(Aged) || name.equals(Backstage)){
+    				quality = (quality<50 == true)?quality+1:quality;
+    			}else{
+    				if(quality>0){
+    					if(!name.equals(Sulfuras)){
+    						quality = quality-1;
+    					}
+    				}else{
+    					quality = 0;
+    				}
+    			}
+    		}
+    	}
     }
 }
